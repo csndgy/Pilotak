5823d720f9e806ea7d46c157232b9f9b91511066
csndgy <gyarmaticsanad@gmail.com>
2023-05-19 10:00:06 +0200

    Commit elotti mentes

---
+++ ConsoleApp1/Pilota.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Pilota
+    {
+        string nev { get; set; }
+        string szuletesiDatum{ get; set; }
+        string nemzetiseg { get; set; }
+        int rajtszam { get; set; }
+        int ev { get; set; }
+
+
+        public Pilota(string elsosor)
+        {
+            var sor = elsosor.Split(";");
+            this.nev = sor[0];
+            this.szuletesiDatum = sor[1];
+            this.nemzetiseg = sor[2];
+            this.rajtszam = int.Parse(sor[3]);
+            this.ev = int.Parse(sor[4]);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using ConsoleApp1;
+using System;
+using System.IO;
+using System.Linq;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace MyApp 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Pilota> pilota = new List<Pilota>();
+            StreamReader sr = new StreamReader("pilotak.csv");
+            sr.ReadLine();
 
+            while (!sr.EndOfStream)
+            {
+                string[] pilotak = sr.ReadLine().Split(";");
+                pilota.Add()
+            }
         }
     }
 }
